c67df692f7a2926f47bb320b7ccd738e568304da
zywu <zywu@Ctrip.com>
2015-06-13 22:58:41 +0800

    spring mvc

--- FirstSpringApp/src/com/firstspringapp/dao/TestDaoImpl.java
+++ FirstSpringApp/src/com/firstspringapp/dao/TestDaoImpl.java
@@ -1,29 +1,39 @@
 package com.firstspringapp.dao;
 
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+import org.springframework.stereotype.Repository;
 
 import com.firstspringapp.bean.DataBean;
 
 
+@Repository
 public class TestDaoImpl  implements TestDao {
-	@Autowired
+	
 	private JdbcTemplate jdbc;
+	
+	@Autowired
+	public TestDaoImpl(JdbcTemplate jdbc) {
+		this.jdbc = jdbc;
+	}
 	@Override
 	public DataBean getDataBeanById(int id) {
-//		return jdbc.queryForObject("select * from ttt where a=?", new ParameterizedRowMapper<DataBean>() {
+//		return jdbc.queryForObject("select * from ttt where a=?", new ParameterizedRowMapper<DataBean>() {		
 //
 //			@Override
-//			public DataBean mapRow(ResultSet rs, int rowNum) throws SQLException {
-//				
+//			public DataBean mapRow(ResultSet arg0, int arg1)
+//					throws SQLException {
 //				DataBean dBean=new DataBean();
-//				dBean.setA(rs.getInt(1));
-//				dBean.setB(rs.getString(2));
+//				dBean.setA(arg0.getInt(1));
+//				dBean.setB(arg0.getString(2));
 //				return dBean;
 //			}
 //		}, id);
